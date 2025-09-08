00f7a39f45a83d7437ec19a2b889f7fc0be95c30
phellipe <phelliperrp@gmail.com>
2025-09-08 13:22:35 -0300

    first commit

---
+++ src/main/java/academy/wakanda/wakacop/WakacopApplication.java
@@ -0,0 +1,13 @@
+package academy.wakanda.wakacop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakacopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakacopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/wakanda/wakacop/WakacopApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.wakanda.wakacop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakacopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
