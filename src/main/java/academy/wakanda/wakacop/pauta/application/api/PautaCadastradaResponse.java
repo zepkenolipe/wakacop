17770219c55763f6e6ab6ee0388b8fee3890cf5f
phellipe <phelliperrp@gmail.com>
2025-09-08 14:50:31 -0300

    fix api response e controller

--- src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaCadastradaResponse.java
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaCadastradaResponse.java
@@ -1,8 +1,13 @@
 package academy.wakanda.wakacop.pauta.application.api;
 
 
+import lombok.Getter;
+import lombok.ToString;
+
 import java.util.UUID;
 
+@Getter
+@ToString
 public class PautaCadastradaResponse {
     private UUID id;
 }
--- src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaController.java
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaController.java
@@ -3,7 +3,6 @@ package academy.wakanda.wakacop.pauta.application.api;
 import academy.wakanda.wakacop.pauta.application.service.PautaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
